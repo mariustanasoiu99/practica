f96e0ea885e853608553f7f3ec01e05ab7ffd31a
Marius Tanasoiu <j.doru86@gmail.com>
2021-08-17 20:22:59 +0300

    Numere de inmatriculare

---
+++ problemeeccpr/13numere_de_inmatriculare.py
@@ -0,0 +1,21 @@
+nr_introduse = []
+nr_valide = []
+
+string1 = ['AB', 'AR', 'AG', 'B', 'BC', 'BH', 'BN', 'BT', 'BV', 'BR', 'BZ', 'CS',
+'CL', 'CJ','CT', 'CV', 'DB', 'DJ', 'GL', 'GR', 'GJ', 'HR', 'HD', 'IL','IS', 'IF','MM', 'MH', 'MS', 'NT', 'OT', 'PH',
+'SM', 'SJ', 'SB', 'SV', 'TR', 'TM', 'TL', 'VS', 'VL', 'VN']
+
+while True:
+    n = input()
+    if n == '':
+        break
+    nr_introduse.append(n)
+
+for i in nr_introduse:
+    if i.split()[0] in string1:
+        if i.split()[1].isdigit() and len(i.split()[1]) in [2, 3]:
+            if i.split()[2].isupper() and len(i.split()[2]) == 3 and i.split()[2].isalpha():
+                nr_valide.append(i)
+
+for i in nr_valide:
+    print(i)
